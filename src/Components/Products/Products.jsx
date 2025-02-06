d0fd03a1b8913d0718b5b8e8ec7877991bfd6250
habibulalam <habibhassan.stt20@gmail.com>
2025-02-06 16:44:37 +0600

    Filter added for products

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,60 +1,102 @@
-
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useProducts } from "../Product Context Api/ProductContext";
 
 const Products = () => {
+  const navigateToDynamicProduct = useNavigate();
+  const { products, isLoading } = useProducts();
+  const [searchName, setSearchName] = useState("");
+  const [searchId, setSearchId] = useState("");
 
-    const navigateToDynamicProduct = useNavigate();
+  const handleSingleProduct = (productId) => {
+    navigateToDynamicProduct(`/singleProduct/${productId}`);
+  };
 
-    // calling the custom hook
-    const { products, isLoading } = useProducts();
-    console.log(products);
+  const handleNameChange = (e) => {
+    setSearchName(e.target.value);
+  };
 
+  const handleIdChange = (e) => {
+    setSearchId(e.target.value);
+  };
 
-    const handleSingleProduct = (productId) => {
-        navigateToDynamicProduct(`/singleProduct/${productId}`)
-    }
-
-    if (isLoading) {
-        return <div className="w-full h-screen flex justify-center items-center"><span className="loading loading-spinner loading-xl"></span></div>;
-    }
-
+  const filteredProducts = products.filter((product) => {
     return (
-        <div>
-            {/* Container */}
-            <div className="my-5">
-                <div className="overflow-x-auto md:overflow-x-hidden rounded-box border border-base-content/55 bg-base-200">
-                    <h1 className="text-center text-2xl font-semibold mb-3 bg-custom-primary text-white pt-2 pb-3 rounded-sm rounded-b-none">Products List</h1>
-                    <table className="table">
-                        {/* head */}
-                        <thead className="border-b border-base-content/55">
-                            <tr className="border-b-2 text-gray-800">
-                                <th></th>
-                                <th>Name</th>
-                                <th>Price</th>
-                                <th>Color</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* row starts */}
-                            {products?.map((product, index) => {
-                                return (
-                                    <tr key={index} onClick={() => handleSingleProduct(product.id)} className="hover:bg-gray-200 hover:cursor-pointer hover:scale-x-[1.02] transition duration-500">
-                                        <th>{index + 1}</th>
-                                        <td>{product.name}</td>
-                                        <td className={`${product.data?.price ? '' : 'text-red-500'}`}>{product.data?.price ? product.data.price+' $': 'Unknown'}</td>
-                                        <td className={`${product.data?.color ? 'text-black' : 'text-red-500'}`}>{product.data?.color ? product.data.color : 'Unknown'}</td>
-                                        <td>...View More</td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+      product.name.toLowerCase().includes(searchName.toLowerCase()) &&
+      (searchId === "" || product.id.toLowerCase().includes(searchId.toLowerCase()))
     );
+  });
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <span className="loading loading-spinner loading-xl"></span>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+        
+      {/* Filter Inputs */}
+      <div className="my-4 flex justify-center items-center gap-4">
+        <h1 className="font-semibold text-lg">Filters :</h1>
+        <input
+          type="text"
+          placeholder="Filter by Name"
+          value={searchName}
+          onChange={handleNameChange}
+          className="input input-bordered placeholder:text-gray-500"
+        />
+        <input
+          type="text"
+          placeholder="Filter by ID"
+          value={searchId}
+          onChange={handleIdChange}
+          className="input input-bordered placeholder:text-gray-500"
+        />
+      </div>
+
+      {/* Container */}
+      <div className="my-5">
+        <div className="overflow-x-auto md:overflow-x-hidden rounded-box border border-base-content/55 bg-base-200">
+          <h1 className="text-center text-2xl font-semibold mb-3 bg-custom-primary text-white pt-2 pb-3 rounded-sm rounded-b-none">
+            Products List
+          </h1>
+          <table className="table">
+            {/* head */}
+            <thead className="border-b border-base-content/55">
+              <tr className="border-b-2 text-gray-800">
+                <th></th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Color</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row starts */}
+              {filteredProducts.map((product, index) => {
+                return (
+                  <tr
+                    key={index}
+                    onClick={() => handleSingleProduct(product.id)}
+                    className="hover:bg-gray-200 hover:cursor-pointer hover:scale-x-[1.02] transition duration-500"
+                  >
+                    <th>{index + 1}</th>
+                    <td>{product.name}</td>
+                    <td className={`${product.data?.price ? '' : 'text-red-500'}`}>{product.data?.price ? product.data.price + ' $' : 'Unknown'}</td>
+                    <td className={`${product.data?.color ? 'text-black' : 'text-red-500'}`}>{product.data?.color ? product.data.color : 'Unknown'}</td>
+                    <td>...View More</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
--- src/Components/Sing Up and Login/SignUp.jsx
+++ src/Components/Sing Up and Login/SignUp.jsx
@@ -1,95 +1,101 @@
 import { useState } from "react";
 import { useAuth } from "../AuthContext/AuthContext";
 import { toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  const { signup } = useAuth();
-  const [formData, setFormData] = useState({ name: "", email: "", password: "" });
+    const { signup } = useAuth();
+    const [formData, setFormData] = useState({ name: "", email: "", password: "" });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+    const navigateToRoot = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const { name, email, password } = formData;
-    const success = signup(name, email, password);
-    if (success) {
-      toast.success('SignUp Successful, now please login!', {
-              position: "top-right",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              theme: "dark",
-              });
-      setFormData({ name: "", email: "", password: "" });
-    } else {
-      toast.error('User already exists. Please choose a different email.', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
         });
-    }
-  };
+    };
 
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <form onSubmit={handleSubmit} className="w-full max-w-lg mx-auto p-4">
-        <fieldset className="bg-base-200 border border-black p-4 rounded-box">
-          <legend className="text-2xl font-semibold mb-4">Signup</legend>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { name, email, password } = formData;
+        const success = signup(name, email, password);
+        if (success) {
+            toast.success('Congratulation! signup Successful ', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
 
-          <label className="block mb-2">Name</label>
-          <input
-            type="text"
-            name="name"
-            className="w-full mb-4 p-2 border border-base-300 rounded"
-            placeholder="Enter your name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
+            setFormData({ name: "", email: "", password: "" });
+            navigateToRoot('/')
 
-          <label className="block mb-2">Email</label>
-          <input
-            type="email"
-            name="email"
-            className="w-full mb-4 p-2 border border-base-300 rounded"
-            placeholder="Enter your email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
+        } else {
+            toast.error('User already exists. Please choose a different email.', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        }
+    };
 
-          <label className="block mb-2">Password</label>
-          <input
-            type="password"
-            name="password"
-            className="w-full mb-4 p-2 border border-base-300 rounded"
-            placeholder="Enter your password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <form onSubmit={handleSubmit} className="w-full max-w-lg mx-auto p-4">
+                <fieldset className="bg-base-200 border border-black p-4 rounded-box">
+                    <legend className="text-2xl font-semibold mb-4">Signup</legend>
 
-          <button type="submit" className="w-full p-2 bg-custom-primary text-white rounded hover:bg-custom-primary-dark">
-            Signup
-          </button>
-        </fieldset>
-      </form>
-    </div>
-  );
+                    <label className="block mb-2">Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        className="w-full mb-4 p-2 border border-base-300 rounded"
+                        placeholder="Enter your name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <label className="block mb-2">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        className="w-full mb-4 p-2 border border-base-300 rounded"
+                        placeholder="Enter your email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <label className="block mb-2">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        className="w-full mb-4 p-2 border border-base-300 rounded"
+                        placeholder="Enter your password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <button type="submit" className="w-full p-2 bg-custom-primary text-white rounded hover:bg-custom-primary-dark">
+                        Signup
+                    </button>
+                </fieldset>
+            </form>
+        </div>
+    );
 };
 
 export default Signup;
